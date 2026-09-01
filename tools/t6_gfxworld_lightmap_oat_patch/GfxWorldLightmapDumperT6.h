183f49b33701ab6ed7227db737a5994daee3a20d
Peter <53446060+lizardpeter@users.noreply.github.com>
2026-09-01 11:30:22 -0500

    Add T6 GfxWorld lightmap OAT dumper header

---
+++ tools/t6_gfxworld_lightmap_oat_patch/GfxWorldLightmapDumperT6.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Dumping/AbstractAssetDumper.h"
+#include "Game/T6/T6.h"
+
+namespace gfx_world_lightmap
+{
+    class DumperT6 final : public AbstractAssetDumper<T6::AssetGfxWorld>
+    {
+    protected:
+        void DumpAsset(
+            AssetDumpingContext& context,
+            const XAssetInfo<T6::AssetGfxWorld::Type>& asset) override;
+    };
+} // namespace gfx_world_lightmap
