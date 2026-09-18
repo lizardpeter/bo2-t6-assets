98bce67c1cabff641f611b3ca7176732d16283e9
Peter <53446060+lizardpeter@users.noreply.github.com>
2026-09-18 15:36:32 -0400

    Emit packed streamed-part count numerically

--- tools/t6_oat_gfximage_packed_identity_trace_patch_v1.py
+++ tools/t6_oat_gfximage_packed_identity_trace_patch_v1.py
@@ -9,7 +9,7 @@ root=Path(sys.argv[1])
 p=root/'src/ObjWriting/Image/ImageToCommonConverter.cpp.template'
 s=p.read_text()
 needle='''#ifdef FEATURE_T6\n        if (image.streamedPartCount > 0)\n        {\n            for (auto* ipak : IIPak::Repository)\n'''
-repl='''#ifdef FEATURE_T6\n        if (image.streamedPartCount > 0)\n        {\n            con::info("T6_GFXIMAGE_PACKED name={} nameHash={} dataHash={} streamedParts={} width={} height={} depth={}", image.name ? image.name : "<null>", image.hash, image.streamedParts[0].hash, image.streamedPartCount, image.width, image.height, image.depth);\n            for (auto* ipak : IIPak::Repository)\n'''
+repl='''#ifdef FEATURE_T6\n        if (image.streamedPartCount > 0)\n        {\n            con::info("T6_GFXIMAGE_PACKED name={} nameHash={} dataHash={} streamedParts={} width={} height={} depth={}", image.name ? image.name : "<null>", image.hash, image.streamedParts[0].hash, static_cast<unsigned>(image.streamedPartCount), image.width, image.height, image.depth);\n            for (auto* ipak : IIPak::Repository)\n'''
 if s.count(needle)!=1: raise SystemExit(f'expected one T6 converter insertion site, got {s.count(needle)}')
 p.write_text(s.replace(needle,repl))
 print(p)
